ef5c11c9068201da4b3ac9bdbe1140c3eb938dcb
Lautaro Zuleta <lautarozule80@gmail.com>
2024-10-14 19:47:07 -0300

    add: endpint post by id

--- Models/Postgres/post.js
+++ Models/Postgres/post.js
@@ -62,11 +62,28 @@ export class PostsModule {
         try {
 
             const res = await connection.query(`
-                select u.username as author, u.surname, u.photo, p.id, p.title, p.content, p.created_at as date, pi2.image_url, c.name as category from posts p
-                inner join authors a on p.id_author = a.id
-                inner join post_image pi2 on p.id = pi2.id_post
-                inner join users u on a.id_user = u.id 
-                inner join categories c on p.id_category = c.id where p.id = $1;
+                    SELECT 
+                        u.username AS author,
+                        u.photo,
+                        a.description,
+                        p.id,
+                        s.name,
+                        p.title,
+                        p.content,
+                        p.created_at,
+                        pi2.image_url,
+                        c.name AS category,
+                        COALESCE(cant_comment.count, 0) AS comment_count,
+                        COALESCE(cant_like.count, 0) AS like_count
+                    FROM posts p
+                    INNER JOIN authors a ON p.id_author = a.id
+                    LEFT JOIN post_image pi2 ON p.id = pi2.id_post
+                    INNER JOIN users u ON a.id_user = u.id 
+                    INNER JOIN categories c ON p.id_category = c.id
+                    inner join states s on p.id_state = s.id
+                    LEFT JOIN (SELECT id_post, COUNT(*) AS count FROM comments GROUP BY id_post) AS cant_comment ON p.id = cant_comment.id_post
+                    LEFT JOIN (SELECT id_post, COUNT(*) AS count FROM post_likes GROUP BY id_post) AS cant_like ON p.id = cant_like.id_post
+                    WHERE p.id = $1;
                 `, [id])
 
             if (res.rowCount == 0) {
